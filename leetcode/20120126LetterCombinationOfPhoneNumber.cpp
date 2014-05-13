52cf2b220f18d764bf9fc96117e5e7d4fe9da725
ibillxia <ibillxia@gmail.com>
2014-05-13 23:52:22 +0800

    add new file to leetcode

--- leetcode/20120126LetterCombinationOfPhoneNumber.cpp
+++ leetcode/20120126LetterCombinationOfPhoneNumber.cpp
@@ -13,37 +13,45 @@ using namespace std;
 
 class Solution {
 public:
-	static char dict[][8] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-	static int len[] = {0,0,3,3,3,3,3,4,3,4};
 	vector<string>ans;
+	string str;
 	vector<string> letterCombinations(string digits) {
-		if(!digits.size())return ans;
-		gen(&digits[0]);
+		if(!digits.size()){
+			ans.push_back("");
+			return ans;
+		}
+		gen(&digits[0],0);
 		return ans;
 	}
 
-	void gen(char *d){
-		int i,j;
-		string str;
-		for(i=0;i<strlen(d);i++){
-			for(j=0;j<len[d[0]-'0'];j++){
-				str+=dict[d[0]-'0'][j];
-				gen(d+1);
-			}
+	void gen(char *d,int n){
+		static const char dict[][8]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+		static const int len[10]={0,0,3,3,3,3,3,4,3,4};
+		int i;
+		if(n==strlen(d)){
+			ans.push_back(str);
+			return;
 		}
-		ans.push_back(str);
+		for(i=0;i<len[d[n]-'0'];i++){
+			str+=dict[d[n]-'0'][i];
+			gen(d,n+1);
+			//str.pop_back();
+			str=str.substr(0,str.size()-1);
+		}
 	}
 };
 
 int main()
 {
 	Solution sol;
-	char str[] = "234";
+	char str[] = "";
 	string digits(str);
+	cout<<digits.size()<<endl;
 	vector<string> ans = sol.letterCombinations(digits);
 	FOR(i,ans.size()){
 		cout<<ans[i]<<endl;
 	}
+	cout<<ans.size()<<endl;
 	return 0;
 }
 
